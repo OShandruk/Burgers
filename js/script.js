6efa12c857452b17f6f16ab3c5b8077e591fc645
Oleksandr Shandruk <sasha_shandruk1991@ukr.net>
2019-02-11 20:51:11 +0200

    Done overlay

--- js/script.js
+++ js/script.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $(".order").hover(function(){
     $(".order-hover").fadeToggle();
   });
+
+
+  $(".button-order").click(function(){
+    $(".order-form").fadeIn();
+    $(".overlay:before").fadeIn();
+  });
 });
