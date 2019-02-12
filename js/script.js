f8e107c13d509a1305eebd734303f6b85a5c404b
Oleksandr Shandruk <sasha_shandruk1991@ukr.net>
2019-02-12 22:21:04 +0200

    Show and Hide order form

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
+
+
   $(".order").hover(function(){
     $(".order-hover").fadeToggle();
   });
 
 
+
   $(".button-order").click(function(){
     $(".order-form").fadeIn();
-    $(".overlay:before").fadeIn();
+    $('#overlay').addClass('new-overlay').fadeIn();
+
   });
+
 });
+
+$(function($){
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".order-form"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+			div.hide();
+			$('#overlay').removeClass(); // скрываем его
+		}
+	});
+});
+  
